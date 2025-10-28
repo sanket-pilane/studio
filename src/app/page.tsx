07b08e9d918058bafb6802e4639818d795e4607c
Lalit Pilane <lalapilane1@gmail.com>
2025-10-28 18:12:55 +0000

    I want you to update the UI like place the AI station finder and the map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,10 +28,10 @@ export default function Home() {
   }
 
   return (
-    <div className="container mx-auto p-4 md:p-8 h-full">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 h-full">
-        <div className="lg:col-span-4 xl:col-span-3 animate-fade-in-up">
-          <Card className="h-full">
+    <div className="w-[80%] mx-auto p-4 md:p-8 h-full">
+      <div className="flex flex-col gap-8 h-full">
+        <div className="animate-fade-in-up">
+          <Card>
             <CardHeader>
               <CardTitle>AI Station Finder</CardTitle>
               <CardDescription>Let our AI find the perfect charging station for you.</CardDescription>
@@ -41,7 +41,7 @@ export default function Home() {
             </CardContent>
           </Card>
         </div>
-        <div className="lg:col-span-8 xl:col-span-9 min-h-[500px] lg:min-h-0 animate-fade-in-up animation-delay-200">
+        <div className="h-[600px] animate-fade-in-up animation-delay-200">
           <MapView />
         </div>
       </div>
