69708b0a6e3c6b97fd7f1c2235632841c8b5d05c
Lalit Pilane <lalapilane1@gmail.com>
2025-10-29 14:37:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4704287603-f1b1f",
+  "appId": "1:407389153009:web:4e37c5a20887b5375dab50",
+  "apiKey": "AIzaSyDJCcpWa1BN_R_aQehwytBXckCLIzCrNz8",
+  "authDomain": "studio-4704287603-f1b1f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "407389153009"
+};
